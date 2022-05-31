97f0dc028f50b54131aad144b6092a43be39f65f
Smerlyn García <smerlyn_alejandro@outlook.com>
2022-05-30 22:07:41 -0400

    Agregando los botones principales

--- Views/MenuPrincipal.Designer.cs
+++ Views/MenuPrincipal.Designer.cs
@@ -28,12 +28,329 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panelIzquierdo = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btnPreplanilla = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.btnPersonal = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.panel9 = new System.Windows.Forms.Panel();
+            this.panel10 = new System.Windows.Forms.Panel();
+            this.panel11 = new System.Windows.Forms.Panel();
+            this.panel12 = new System.Windows.Forms.Panel();
+            this.panel13 = new System.Windows.Forms.Panel();
+            this.panel14 = new System.Windows.Forms.Panel();
+            this.panel15 = new System.Windows.Forms.Panel();
+            this.btnRespaldo = new System.Windows.Forms.Button();
+            this.btnRestaurarDB = new System.Windows.Forms.Button();
+            this.btnUsuario = new System.Windows.Forms.Button();
+            this.btnRegistro = new System.Windows.Forms.Button();
+            this.panelIzquierdo.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel7.SuspendLayout();
+            this.panel10.SuspendLayout();
+            this.panel13.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelIzquierdo
+            // 
+            this.panelIzquierdo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(19)))), ((int)(((byte)(19)))));
+            this.panelIzquierdo.Controls.Add(this.btnPersonal);
+            this.panelIzquierdo.Controls.Add(this.btnPreplanilla);
+            this.panelIzquierdo.Controls.Add(this.panel3);
+            this.panelIzquierdo.Controls.Add(this.panel4);
+            this.panelIzquierdo.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelIzquierdo.Location = new System.Drawing.Point(0, 0);
+            this.panelIzquierdo.Name = "panelIzquierdo";
+            this.panelIzquierdo.Size = new System.Drawing.Size(222, 826);
+            this.panelIzquierdo.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(222, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(959, 826);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.panel13);
+            this.panel3.Controls.Add(this.panel10);
+            this.panel3.Controls.Add(this.panel7);
+            this.panel3.Controls.Add(this.panel1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel3.Location = new System.Drawing.Point(0, 296);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(222, 530);
+            this.panel3.TabIndex = 0;
+            // 
+            // btnPreplanilla
+            // 
+            this.btnPreplanilla.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(19)))), ((int)(((byte)(19)))));
+            this.btnPreplanilla.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnPreplanilla.FlatAppearance.BorderSize = 0;
+            this.btnPreplanilla.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPreplanilla.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPreplanilla.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnPreplanilla.Location = new System.Drawing.Point(0, 104);
+            this.btnPreplanilla.Name = "btnPreplanilla";
+            this.btnPreplanilla.Size = new System.Drawing.Size(222, 83);
+            this.btnPreplanilla.TabIndex = 1;
+            this.btnPreplanilla.Text = "Pre planilla";
+            this.btnPreplanilla.UseVisualStyleBackColor = false;
+            // 
+            // panel4
+            // 
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel4.Location = new System.Drawing.Point(0, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(222, 104);
+            this.panel4.TabIndex = 2;
+            // 
+            // btnPersonal
+            // 
+            this.btnPersonal.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(19)))), ((int)(((byte)(19)))));
+            this.btnPersonal.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnPersonal.FlatAppearance.BorderSize = 0;
+            this.btnPersonal.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPersonal.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPersonal.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnPersonal.Location = new System.Drawing.Point(0, 187);
+            this.btnPersonal.Name = "btnPersonal";
+            this.btnPersonal.Size = new System.Drawing.Size(222, 83);
+            this.btnPersonal.TabIndex = 3;
+            this.btnPersonal.Text = "Personal";
+            this.btnPersonal.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnRegistro);
+            this.panel1.Controls.Add(this.panel6);
+            this.panel1.Controls.Add(this.panel5);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(222, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel5
+            // 
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel5.Location = new System.Drawing.Point(0, 0);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(222, 10);
+            this.panel5.TabIndex = 0;
+            // 
+            // panel6
+            // 
+            this.panel6.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel6.Location = new System.Drawing.Point(0, 10);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(10, 90);
+            this.panel6.TabIndex = 1;
+            // 
+            // panel7
+            // 
+            this.panel7.Controls.Add(this.btnUsuario);
+            this.panel7.Controls.Add(this.panel8);
+            this.panel7.Controls.Add(this.panel9);
+            this.panel7.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel7.Location = new System.Drawing.Point(0, 100);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(222, 100);
+            this.panel7.TabIndex = 1;
+            // 
+            // panel8
+            // 
+            this.panel8.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel8.Location = new System.Drawing.Point(0, 10);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(10, 90);
+            this.panel8.TabIndex = 1;
+            // 
+            // panel9
+            // 
+            this.panel9.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel9.Location = new System.Drawing.Point(0, 0);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(222, 10);
+            this.panel9.TabIndex = 0;
+            // 
+            // panel10
+            // 
+            this.panel10.Controls.Add(this.btnRestaurarDB);
+            this.panel10.Controls.Add(this.panel11);
+            this.panel10.Controls.Add(this.panel12);
+            this.panel10.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel10.Location = new System.Drawing.Point(0, 200);
+            this.panel10.Name = "panel10";
+            this.panel10.Size = new System.Drawing.Size(222, 100);
+            this.panel10.TabIndex = 2;
+            // 
+            // panel11
+            // 
+            this.panel11.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel11.Location = new System.Drawing.Point(0, 10);
+            this.panel11.Name = "panel11";
+            this.panel11.Size = new System.Drawing.Size(10, 90);
+            this.panel11.TabIndex = 1;
+            // 
+            // panel12
+            // 
+            this.panel12.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel12.Location = new System.Drawing.Point(0, 0);
+            this.panel12.Name = "panel12";
+            this.panel12.Size = new System.Drawing.Size(222, 10);
+            this.panel12.TabIndex = 0;
+            // 
+            // panel13
+            // 
+            this.panel13.Controls.Add(this.btnRespaldo);
+            this.panel13.Controls.Add(this.panel14);
+            this.panel13.Controls.Add(this.panel15);
+            this.panel13.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel13.Location = new System.Drawing.Point(0, 300);
+            this.panel13.Name = "panel13";
+            this.panel13.Size = new System.Drawing.Size(222, 100);
+            this.panel13.TabIndex = 3;
+            // 
+            // panel14
+            // 
+            this.panel14.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel14.Location = new System.Drawing.Point(0, 10);
+            this.panel14.Name = "panel14";
+            this.panel14.Size = new System.Drawing.Size(10, 90);
+            this.panel14.TabIndex = 1;
+            // 
+            // panel15
+            // 
+            this.panel15.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel15.Location = new System.Drawing.Point(0, 0);
+            this.panel15.Name = "panel15";
+            this.panel15.Size = new System.Drawing.Size(222, 10);
+            this.panel15.TabIndex = 0;
+            // 
+            // btnRespaldo
+            // 
+            this.btnRespaldo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(19)))), ((int)(((byte)(19)))));
+            this.btnRespaldo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnRespaldo.FlatAppearance.BorderSize = 0;
+            this.btnRespaldo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRespaldo.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRespaldo.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnRespaldo.Image = global::UsuariosPaginadoApp_CSharp.Properties.Resources.respaldo;
+            this.btnRespaldo.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnRespaldo.Location = new System.Drawing.Point(10, 10);
+            this.btnRespaldo.Name = "btnRespaldo";
+            this.btnRespaldo.Size = new System.Drawing.Size(212, 90);
+            this.btnRespaldo.TabIndex = 4;
+            this.btnRespaldo.Text = "Respaldo";
+            this.btnRespaldo.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnRespaldo.UseVisualStyleBackColor = false;
+            // 
+            // btnRestaurarDB
+            // 
+            this.btnRestaurarDB.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(19)))), ((int)(((byte)(19)))));
+            this.btnRestaurarDB.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnRestaurarDB.FlatAppearance.BorderSize = 0;
+            this.btnRestaurarDB.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRestaurarDB.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRestaurarDB.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnRestaurarDB.Image = global::UsuariosPaginadoApp_CSharp.Properties.Resources.clasificado;
+            this.btnRestaurarDB.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnRestaurarDB.Location = new System.Drawing.Point(10, 10);
+            this.btnRestaurarDB.Name = "btnRestaurarDB";
+            this.btnRestaurarDB.Size = new System.Drawing.Size(212, 90);
+            this.btnRestaurarDB.TabIndex = 4;
+            this.btnRestaurarDB.Text = "RestaurarDB";
+            this.btnRestaurarDB.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnRestaurarDB.UseVisualStyleBackColor = false;
+            // 
+            // btnUsuario
+            // 
+            this.btnUsuario.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(19)))), ((int)(((byte)(19)))));
+            this.btnUsuario.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnUsuario.FlatAppearance.BorderSize = 0;
+            this.btnUsuario.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUsuario.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnUsuario.Image = global::UsuariosPaginadoApp_CSharp.Properties.Resources.usuario;
+            this.btnUsuario.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnUsuario.Location = new System.Drawing.Point(10, 10);
+            this.btnUsuario.Name = "btnUsuario";
+            this.btnUsuario.Size = new System.Drawing.Size(212, 90);
+            this.btnUsuario.TabIndex = 4;
+            this.btnUsuario.Text = "Usuarios";
+            this.btnUsuario.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnUsuario.UseVisualStyleBackColor = false;
+            // 
+            // btnRegistro
+            // 
+            this.btnRegistro.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(19)))), ((int)(((byte)(19)))));
+            this.btnRegistro.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnRegistro.FlatAppearance.BorderSize = 0;
+            this.btnRegistro.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRegistro.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegistro.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnRegistro.Image = global::UsuariosPaginadoApp_CSharp.Properties.Resources.reloj;
+            this.btnRegistro.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnRegistro.Location = new System.Drawing.Point(10, 10);
+            this.btnRegistro.Name = "btnRegistro";
+            this.btnRegistro.Size = new System.Drawing.Size(212, 90);
+            this.btnRegistro.TabIndex = 4;
+            this.btnRegistro.Text = "Registro";
+            this.btnRegistro.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnRegistro.UseVisualStyleBackColor = false;
+            // 
+            // MenuPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1181, 826);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panelIzquierdo);
+            this.Name = "MenuPrincipal";
             this.Text = "MenuPrincipal";
+            this.panelIzquierdo.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel7.ResumeLayout(false);
+            this.panel10.ResumeLayout(false);
+            this.panel13.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panelIzquierdo;
+        private System.Windows.Forms.Button btnPersonal;
+        private System.Windows.Forms.Button btnPreplanilla;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel13;
+        private System.Windows.Forms.Button btnRespaldo;
+        private System.Windows.Forms.Panel panel14;
+        private System.Windows.Forms.Panel panel15;
+        private System.Windows.Forms.Panel panel10;
+        private System.Windows.Forms.Button btnRestaurarDB;
+        private System.Windows.Forms.Panel panel11;
+        private System.Windows.Forms.Panel panel12;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Button btnUsuario;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Panel panel9;
+        private System.Windows.Forms.Button btnRegistro;
     }
 }
